c2d9fbad2c8e8d6fdcb7550288c532478d0c32c6
Itishree Mandal <itishreemandal91@gmail.com>
2026-08-19 16:49:22 -0600

    Add Terraform + Github Actions pipeline

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-itishree"
+    key    = "website/terraform.tfstate"
+    region = "us-east-2”
+  }
+}
+
+provider "aws" {
+  region = "us-east-2”
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = "Jayaguru”
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+  index_document { suffix = "index.html" }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket                  = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.website.arn}/*"
+    }]
+  })
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
+}
